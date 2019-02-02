fdc28096ffb98539b5dba603eade383961426e75
Riccardo <riki-95@hotmail.it>
2019-02-02 12:15:44 +0100

    Write to CSV

--- twitch-stats.py
+++ twitch-stats.py
@@ -1,5 +1,6 @@
 from secrets import client_id, OAuth
 from twitch_api_service import usersget, followersget
+import csv
 
 headers = {
     'Client-ID': client_id,
@@ -12,7 +13,11 @@ users = [
 ]
 
 userIDs = usersget(headers,users)
-print(userIDs)
 
 followers_count = followersget(headers, userIDs)
-print(followers_count)
+
+with open('twitch.csv', mode='w') as twitchcsv:
+    twitch_writer = csv.writer(twitchcsv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    twitch_writer.writerow(['Username', 'UserID', 'Followers'])
+    for i in range(len(users)):
+        twitch_writer.writerow([users[i], userIDs[i], followers_count[i]])
