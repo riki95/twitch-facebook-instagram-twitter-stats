42e5887be6ea28c2aaa8d0b325fd29980270c3bc
Riccardo <riki-95@hotmail.it>
2019-02-03 11:11:51 +0100

    Added header to instagram

--- instagram_api_service.py
+++ instagram_api_service.py
@@ -1,7 +1,17 @@
 import requests
+from secrets import token_IG, client_id_IG
+import urllib.request
+
+headers = {
+    'access_token': token_IG,
+
+}
 
 def followersget(users):
     for user in users:
-        response = requests.get('https://www.instagram.com/' + user + '/?__a=1')
+        response = requests.get('https://www.instagram.com/' + user + '/?__a=1', headers=headers)
+
         print(response)
     return
+
+followersget(['Bloor95'])
